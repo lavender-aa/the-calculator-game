9833fcbbf905ddee9ebdd0a401103ccc1240b490
lavender-aa <sodaissoda@gmail.com>
2025-04-26 01:47:26 -0400

    forgot to add 0 button into numbered buttons list, added section in windows batch installer since pyinstaller may not be put onto path on install

--- install.bat
+++ install.bat
@@ -11,4 +11,14 @@ python -m pip install pyinstaller sympy
 
 :: create binary
 echo Buliding game binary
-pyinstaller --noconfirm --onefile --noconsole src\game.py
+pyinstaller --noconfirm --onefile --noconsole src\game.py || (
+    :: pyinstaller not found
+    echo Build failed; please ensure PyInstaller's parent folder is included in your PATH.
+    echo This folder is in one of two places; run these lines inside a python environment to find them:
+    echo
+    echo 1) >>> import sysconfig; print(sysconfig.get_path("scripts"))
+    echo 2) >>> import site; print(site.USER_BASE + "\\Scripts")
+    echo
+    echo Open both of these folders; whichever one has `pyinstaller.exe`, add that one to your system or user PATH, and try installing again.
+    pause
+)
