76358f1ee9829b12b08d93a8fc7830ecab80a77e
lavender-aa <sodaissoda@gmail.com>
2025-04-26 01:24:21 -0400

    changed windows batch script to use python instead of python3, added pause for if user doesn't have python installed

--- install.bat
+++ install.bat
@@ -1,12 +1,13 @@
 :: make sure python exists
-python3 --version || (
+python --version || (
     echo Python3 is not installed; cancelling install.
+    pause
     exit
 )
 
 :: install dependencies
 echo Installing dependencies (pyintaller, sympy)
-python3 -m pip install pyinstaller sympy
+python -m pip install pyinstaller sympy
 
 :: create binary
 echo Buliding game binary
